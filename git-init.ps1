aa3951c593f16e43695d2ad49aa1de44ffb01832
huhuiyu <1069306849@qq.com>
2025-06-29 08:54:04 +0800

    项目初始化

---
+++ git-init.ps1
@@ -0,0 +1,5 @@
+# github仓库地址：git@github.com:huhuiyu/ncre.git
+# 移除gitee仓库地址
+git remote remove gitee
+# 添加gitee仓库地址
+git remote add gitee git@gitee.com:huhuiyu/ncre.git
---
+++ push.ps1
@@ -0,0 +1,7 @@
+# 多仓库推送
+git pull
+Write-Output "===========pull finish==========="
+git push
+Write-Output "===========push github finish==========="
+git push gitee
+Write-Output "===========push gitee finish==========="
